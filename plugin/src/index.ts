ee1bae72659ca8ea1caf45e42e85fa730f9244e7
Loris Sigrist <43482866+LorisSigrist@users.noreply.github.com>
2022-08-12 19:43:16 +0200

    Added enforce:'pre'

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -2,8 +2,9 @@ import type { Plugin } from "vite"
 
 export default function vitePluginPartial() : Plugin{
     return {
-        "name" : "vite-plugin-partial",
-        "transformIndexHtml" : (html)=> {
+        name : "vite-plugin-partial",
+        enforce: 'pre', //We want to run before vite does it's thing, so that any scripts that are added by partials are also transformed
+        transformIndexHtml : (html)=> {
             return html.replace("Please Replace this", "I have replaced it");
         }
     }
