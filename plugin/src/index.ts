ddd7019395aaffc9575555e0fed6cd331f792aad
Loris Sigrist <43482866+LorisSigrist@users.noreply.github.com>
2022-08-12 23:17:44 +0200

    Finished setting up tests

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -5,7 +5,7 @@ export default function vitePluginPartial() : Plugin{
         name : "vite-plugin-partial",
         enforce: 'pre', //We want to run before vite does it's thing, so that any scripts that are added by partials are also transformed
         transformIndexHtml : (html)=> {
-            return html.replace("Please Replace this", "I have replaced it");
+            return html.replace("Please Replace this", "I have replaced it some more");
         }
     }
 }
