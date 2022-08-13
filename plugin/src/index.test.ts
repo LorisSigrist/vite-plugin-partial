33a168be321b6d27174f01ab4c97a82873f2558a
Loris Sigrist <43482866+LorisSigrist@users.noreply.github.com>
2022-08-13 11:49:42 +0200

    Enforce pre is now mandatory

--- plugin/src/index.test.ts
+++ plugin/src/index.test.ts
@@ -1,9 +1,14 @@
 import { it, describe, expect } from 'vitest'
 import vitePluginPartial from '.'
 
-describe('vite-plugin-partial', () => {
-    it("has the name 'vite-plugin-partial'", () => {
+describe('Plugin configuration', () => {
+  it("has the name 'vite-plugin-partial'", () => {
     const plugin = vitePluginPartial()
     expect(plugin.name).toBe('vite-plugin-partial')
+  })
+
+  it('Runs before the vite core modules', () => {
+    const plugin = vitePluginPartial()
+    expect(plugin.enforce).toBe('pre')
   })
 })
