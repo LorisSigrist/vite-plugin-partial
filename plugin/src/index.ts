ee701be0c735e1e429de234349789feaeeea8799
Loris Sigrist <43482866+LorisSigrist@users.noreply.github.com>
2022-08-14 12:17:18 +0200

    Fixed root directory resolution

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -2,12 +2,22 @@ import type { Plugin } from "vite"
 import transformHtml from "./insertPartials/transformHTML"
 
 export default function vitePluginPartial() : Plugin{
+    const options = {
+        root: process.cwd(),
+      };
+
+
     return {
         name : "vite-plugin-partial",
         enforce: 'pre', //We want to run before vite does it's thing, so that any scripts that are added by partials are also transformed
+        
+        configResolved(config) {
+            options.root = config.root;
+        },
+        
         transformIndexHtml: async (html, ctx)=> {
             const filePath = ctx.filename
-            const serverRoot = ctx.server?.config.root
+            const serverRoot = options.root;
             if (!serverRoot) throw "Could not resolve vite's base directory"
             return await transformHtml(html, filePath, serverRoot);
         }
