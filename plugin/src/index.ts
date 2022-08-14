82e2ea3050f4fe2eec2403cffaa66a3702ab1e93
Loris Sigrist <43482866+LorisSigrist@users.noreply.github.com>
2022-08-14 12:17:36 +0200

    Formatting...

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,25 +1,24 @@
-import type { Plugin } from "vite"
-import transformHtml from "./insertPartials/transformHTML"
+import { Plugin } from 'vite'
+import transformHtml from './insertPartials/transformHTML'
 
-export default function vitePluginPartial() : Plugin{
-    const options = {
-        root: process.cwd(),
-      };
+export default function vitePluginPartial (): Plugin {
+  const options = {
+    root: process.cwd()
+  }
 
+  return {
+    name: 'vite-plugin-partial',
+    enforce: 'pre', //We want to run before vite does it's thing, so that any scripts that are added by partials are also transformed
 
-    return {
-        name : "vite-plugin-partial",
-        enforce: 'pre', //We want to run before vite does it's thing, so that any scripts that are added by partials are also transformed
-        
-        configResolved(config) {
-            options.root = config.root;
-        },
-        
-        transformIndexHtml: async (html, ctx)=> {
-            const filePath = ctx.filename
-            const serverRoot = options.root;
-            if (!serverRoot) throw "Could not resolve vite's base directory"
-            return await transformHtml(html, filePath, serverRoot);
-        }
+    configResolved (config) {
+      options.root = config.root
+    },
+
+    transformIndexHtml: async (html, ctx) => {
+      const filePath = ctx.filename
+      const serverRoot = options.root
+      if (!serverRoot) throw "Could not resolve vite's base directory"
+      return await transformHtml(html, filePath, serverRoot)
     }
+  }
 }
